7367d7a8f776c81df8d2da0373d53086ba189cc1
Arthur SKOBNIKOV <arthur.skobnikov@gmail.com>
2025-09-13 12:49:22 +0200

    refactor main data ingestion

--- data_ingestion/main.py
+++ data_ingestion/main.py
@@ -1,4 +1,3 @@
-
 import json
 import os
 
@@ -7,9 +6,13 @@ from extractors.ibkr.IBAccountSummary import main as get_ibkr_account_summary
 from loaders.local_datalake import write_json_to_datalake
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-LOCAL_DATALAKE_PATH = os.path.join(PROJECT_ROOT, "datalake")
+LOCAL_DATALAKE_PATH = os.getenv("LOCAL_DATALAKE_PATH", os.path.join(PROJECT_ROOT, "datalake"))
 
-data = get_ibkr_account_summary()
+def main():
+    # Extract data from IBKR and write to local datalake
+    ibkr_account_summary = get_ibkr_account_summary()
+    write_json_to_datalake(ibkr_account_summary, namespace="ibkr", stream="IBAccountSummary", datalake_root=LOCAL_DATALAKE_PATH)
 
-write_json_to_datalake(data, namespace="ibkr", stream="IBAccountSummary", datalake_root=LOCAL_DATALAKE_PATH)
-print("Data written to local datalake.")
+
+if __name__ == "__main__":
+    main()
